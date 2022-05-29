102a5f65a19b7f17752c7557ef3b3f82593070f8
Tillwege <tillwege@gmail.com>
2022-05-29 22:39:52 +0200

    random shit test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
+    let a = Color::new(8, 1,8);
+    let b: Color = Color::new(255,0,0);
+    let c: Color = Color::add_color(&a, &b);
     println!("Hello, world!");
+    println!("A: {},{},{}",a.r,a.b,a.g);
+    println!("B: {},{},{}",b.r,b.b,b.g);
+    println!("C: {},{},{}",c.r,c.b,c.g);
+}
+
+#[derive(Debug)]
+struct Color{
+    r: u8,
+    g: u8,
+    b: u8
+}
+
+impl Color{
+
+    fn new(r: u8, g: u8, b: u8) -> Color{
+        Color{r, g, b}
+    }
+
+    fn add_color(&self, p_color: &Color) -> Color {
+        let new_r = u8::saturating_add(self.r, p_color.r);
+        let new_g = u8::saturating_add(self.g, p_color.g);
+        let new_b = u8::saturating_add(self.b, p_color.b);
+
+        Color{r: new_r, g: new_g, b: new_b}
+    }
+}
+
+#[test]
+fn add_test(){
+    let result: u8 = 2 + 2;
+    assert_eq!(result, 4);
 }
