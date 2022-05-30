33dd9c1bb5fa6423c8e046dbae61f3d24d87c8da
Tillwege <tillwege@gmail.com>
2022-05-30 19:37:35 +0200

    Hello World with Iced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use iced::{executor, Application, Command, Element, Settings, Text};
+
 fn main() {
-    println!("Hello, world!");
+    let result = Game::run(Settings::default());
+    match result
+    {
+        Ok(_suc) => println!("Success"),
+        Err(err) => println!("Error {}", err),
+    };
+}
+
+struct Game;
+
+impl Application for Game {
+    type Executor = executor::Default;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Game, Command<Self::Message>) {
+        (Game, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Game of Life in Rust")
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
 }
