931e92e20262c0bfbd4fcc50eb6921e0e747705a
Tillwege <tillwege@gmail.com>
2022-06-09 13:14:03 +0200

    works v1

---
+++ src/game.rs
@@ -0,0 +1,133 @@
+use eframe::epaint::Color32;
+use rand::Rng;
+
+use self::Direction::*;
+
+enum Direction {
+    TopLeft,
+    Top,
+    TopRight,
+    Left,
+    Right,
+    BottomLeft,
+    Bottom,
+    BottomRight
+}
+
+impl Direction {
+
+    pub fn to_offset(&self) -> (isize, isize) {
+        match self {
+            Direction::TopLeft => (-1, -1),
+            Direction::Top => (0, -1),
+            Direction::TopRight => (1, -1),
+            Direction::Left => (-1, 0),
+            Direction::Right => (1, 0),
+            Direction::BottomLeft => (-1, 1),
+            Direction::Bottom => (0, 1),
+            Direction::BottomRight => (1, 1),
+        }
+    }
+
+    pub fn iterator() -> std::slice::Iter<'static, Direction> {
+        static DIRECTIONS: [Direction; 8] = [TopLeft, Top, TopRight, Left, Right, BottomLeft, Bottom, BottomRight];
+        DIRECTIONS.iter()
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Game {
+    height: isize,
+    width: isize,
+    state: Vec<bool>,
+}
+
+impl Game {
+    
+    pub fn new(width: isize, height: isize) -> Self {
+        let mut state = Vec::with_capacity((width * height) as usize);
+
+        let mut rng = rand::thread_rng();
+        for _i in 0..(width * height) {
+            state.push(rng.gen());
+        }
+        
+        Game{height, width, state}
+    }
+
+
+    #[inline]
+    pub fn index_of(&mut self, x: isize, y: isize) -> usize {
+        return (x + (self.width * y)) as usize;
+    }
+
+    pub fn get_value(&mut self, x: isize, y: isize) -> Option<bool> {
+        if x>0 && x < self.width && y > 0 && y < self.height {
+            let pos = self.index_of(x, y);
+            Some(self.state[pos])
+        } else {
+            None
+        }
+    }
+
+    pub fn get_value_by_index(&mut self, index: usize) -> Option<bool> {
+        if index>0 && index< self.state.len() {
+            Some(self.state[index])
+        }else{
+            None
+        }
+    }
+
+    pub fn update(&mut self){
+        for x in 0..self.width {
+            for y in 0..self.height {
+                let index = self.index_of(x, y);
+
+
+                // ------------------------------------------//
+
+                let mut neighbours_alive = 0;
+
+                // count neighbours
+
+                for dir in Direction::iterator(){
+                    let offset = dir.to_offset();
+                    if self.get_value(x + offset.0, y + offset.1).unwrap_or_else(||{false}) {
+                       neighbours_alive = neighbours_alive + 1;
+                    }
+                }
+                // ------------------------------------------//
+
+                // apply rules
+                let new_val = match &self.state[index] {
+                    true => match neighbours_alive {
+                        2|3 => true,
+                        _ => false
+                    },
+                    false => match neighbours_alive {
+                        3 => true,
+                        _ => false
+                    },
+                };
+
+                // ------------------------------------------//
+
+                // rewrite to array
+                self.state[index] = new_val;
+            }
+        }
+    }
+
+    // pub fn show(&mut self, pixels: &mut Vec<Color32>){
+    //     for i in 0..self.state.len() {
+    //         let val = &self.state[i];
+    //         if *val {
+    //             pixels[i] = Color32::BLACK;
+    //         }else{
+    //             pixels[i] = Color32::WHITE;
+    //         }
+    //     }
+
+    // }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::{egui::{self, widgets, Ui, Image}, emath::Pos2, epaint::{Stroke, Rounding, ColorImage, ImageData, Color32, TextureHandle, Shape}};
+mod game;
+
+use eframe::{
+    egui::{self, widgets, },
+    epaint::{Color32, ColorImage, ImageData},
+};
+use game::Game;
 
 fn main() {
     let mut options = eframe::NativeOptions::default();
-    options.vsync = false;
-    let App = RustOfLife::default();
-    eframe::run_native(
-        "Rust of Life",
-        options,
-        Box::new(|_cc| Box::new(App)),
-    );
+    options.vsync = true;
+    let app = RustOfLife::default();
+     eframe::run_native("Rust of Life", 
+                         options, 
+                         Box::new(|_cc| Box::new(app)));
 }
 
 struct RustOfLife {
     width: usize,
     height: usize,
+    game: Option<Game>,
     texture: Option<egui::TextureHandle>,
     frames: usize,
     starttime: std::time::SystemTime,
@@ -26,9 +31,10 @@ impl Default for RustOfLife {
         Self {
             width: 100,
             height: 100,
+            game: None,
             texture: None,
             frames: 0,
-            starttime: std::time::SystemTime::now()
+            starttime: std::time::SystemTime::now(),
         }
     }
 }
@@ -49,49 +55,67 @@ impl eframe::App for RustOfLife {
             });
             ui.separator();
             if ui.button("Start Game").clicked() {
-                self.init(ctx);
+                self.init_game();
             }
         });
 
-
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-
-            match &self.texture {
-                Some(tex) => {
-                    ui.image(tex, tex.size_vec2());
-                },
-                None => (),
+        egui::CentralPanel::default().show(ctx, |ui| match &self.texture {
+            Some(tex) => {
+                ui.image(tex, tex.size_vec2());
             }
+            None => (),
         });
+
+        if self.game.is_some() {
+            self.game.as_mut().unwrap().update();
+            self.update_texture(ctx);
+        }
 
         self.print_fps();
         ctx.request_repaint();
     }
 }
 
-impl RustOfLife{
-    
-    fn init(&mut self, ctx: &egui::Context)
-    {
-        let clr_img: ColorImage = ColorImage::new([self.width, self.height], Color32::BLACK);
-        let img_data = ImageData::Color(clr_img);
+impl RustOfLife {
+    fn init_game(&mut self) {
+        let i_width  = isize::try_from(self.width).ok().unwrap_or_else(||0);
+        let i_height = isize::try_from(self.height).ok().unwrap_or_else(||0);
         
+        self.game = Some(Game::new(i_width, i_height));
+    }
+
+
+    fn update_texture(&mut self, ctx: &egui::Context){
+        if self.game.is_none() { return };
+        
+        let mut clr_img: ColorImage = ColorImage::new([self.width, self.height], Color32::BLACK);
+        //let pixels = &clr_img.pixels;
+        //self.game.as_mut().unwrap().show(&mut pixels);
+        for i in 0..clr_img.pixels.len() {
+            let val = self.game.as_mut().unwrap().get_value_by_index(i);
+            if val.unwrap_or_else(||false) {
+                clr_img.pixels[i] = Color32::BLACK;
+            }else{
+                clr_img.pixels[i] = Color32::WHITE;
+            }
+        }
+
+        let img_data = ImageData::Color(clr_img);
+
         self.texture = Some(ctx.load_texture("game_img", img_data));
     }
 
-    fn print_fps(&mut self)
-    {
+
+    fn print_fps(&mut self) {
         self.frames = self.frames + 1;
-        
+
         match self.starttime.elapsed() {
             Ok(runtime) => {
-                let time =  std::cmp::max(runtime.as_secs(), 1);
+                let time = std::cmp::max(runtime.as_secs(), 1);
 
                 let fps = (self.frames as u64) / time;
                 println!("Current FPS: {}", fps);
-
-            },
+            }
             Err(err) => println!("stuff went wrong while calculating fps {}", err),
         }
     }
